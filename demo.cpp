28ac2b7cf844524c0fc24128e24da3b0c059d279
Ori Goldfryd <117982672+OriGoldfrydCS@users.noreply.github.com>
2024-06-14 02:32:03 +0300

    v5

--- demo.cpp
+++ demo.cpp
@@ -26,7 +26,9 @@ int main()
     tree.add_sub_node(n1, n3);
     tree.add_sub_node(n1, n4);
     tree.add_sub_node(n2, n5);
-   
+    
+    cout << tree;   // Should print the graph using GUI.
+
     // The tree should look like:
     /**
      *       root = 1.1
@@ -39,46 +41,47 @@ int main()
     cout << "Pre-order traversal:" << endl;
     for (auto node = tree.begin_pre_order(); node != tree.end_pre_order(); ++node)
     {
-        cout << node->get_value() << endl;
+        cout << node->get_value() << " ";
     } // prints: 1.1, 1.2, 1.4, 1.5, 1.3, 1.6
     
     cout << "\nPost-order traversal:" << endl;
     for (auto node = tree.begin_post_order(); node != tree.end_post_order(); ++node)
     {
-        cout << node->get_value() << endl;
+        cout << node->get_value() << " ";
     } // prints: 1.4, 1.5, 1.2, 1.6, 1.3, 1.1
 
     cout << "\nIn-order traversal:" << endl;
     for (auto node = tree.begin_in_order(); node != tree.end_in_order(); ++node)
     {
-        cout << node->get_value() << endl;
+        cout << node->get_value() << " ";
     } // prints: 1.4, 1.2, 1.5, 1.1, 1.6, 1.3
 
     cout << "\nBFS traversal:" << endl;
     for (auto node = tree.begin_bfs_scan(); node != tree.end_bfs_scan(); ++node)
     {
-        cout << node->get_value() << endl;
+        cout << node->get_value() << " ";
     } // prints: 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
 
     cout << "\nDFS traversal:" << endl;
     for (auto node = tree.begin_dfs_scan(); node != tree.end_dfs_scan(); ++node)
     {
-        cout << node->get_value() << endl;
+        cout << node->get_value() << " ";
     } // prints: 1.1, 1.2, 1.4, 1.5, 1.3, 1.6 
 
     cout << "\nGenaral itertor:" << endl;
     for (auto node : tree)
     {
-        cout << node.get_value() << endl;
+        cout << node.get_value() << " ";
     } // same as BFS: 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
 
     cout << "\nHeap traversal:" << endl;
     for (auto node = tree.myHeap(); node != tree.end_heap(); ++node)
     {
-        cout << node->get_value() << endl;
+        cout << node->get_value() << " ";
     } // prints: 1.1, 1.2, 1.3, 1.4, 1.5, 1.6
 
-    cout << tree;   // Should print the graph using GUI.
+    cout << " " << endl;
+
 
     Tree<int, 3> three_ary_tree;
     Node<int, 3>* root_node_3 = new Node<int, 3>(1);
@@ -95,7 +98,7 @@ int main()
     three_ary_tree.add_sub_node(n1_3, n4_3);
     three_ary_tree.add_sub_node(n2_3, n5_3);
 
-    // cout << three_ary_tree; // Should print the graph using GUI.
+    cout << three_ary_tree; // Should print the graph using GUI.
 
      // The tree should look like:
     /**
